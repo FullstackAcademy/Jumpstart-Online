60b55919d7a26f70d659c2308495a2a0fb34d252
Scott D'Alessandro <sdaless22@gmail.com>
2016-02-12 18:44:21 -0500

    Update underscore_toCamelCase.js

--- Problem-Set-2-Solutions/underscore_toCamelCase.js
+++ Problem-Set-2-Solutions/underscore_toCamelCase.js
@@ -1,42 +1,49 @@
-function underToCamel(name){
-	var finalString = "",
-		name = name.toLowerCase(),
-		count = 0;
-
-	for(var i = 0; i < name.length; i++){
-		if(name[i] === "_"){
-			finalString += name[i+1].toUpperCase();
-			count++;
-		} else if(finalString[i-count] === name[i].toUpperCase()) {
-		    continue;
-		} else {
-		    finalString += name[i];
-		}
-	}
-	return finalString;
+function underToCamel(underName) {
+    var camelCaseOutput = "";
+    var foundUnder = false;
+    for(var i = 0; i<underName.length; i++) {
+        // underName[i];
+        if (underName[i] === "_") {
+            foundUnder = true;
+        } else {
+            if (foundUnder) {
+                camelCaseOutput += underName[i].toUpperCase();
+                foundUnder = false;
+            } else {
+                camelCaseOutput += underName[i];
+            }
+        }
+    }
+  return camelCaseOutput;
 }
 
 
-// function underToCamel(underName) {
-//     var camelCaseOutput = "";
-    
-//     /* Solution 2 */    
-//     var foundUnder = false;
-//     for(var i = 0; i<underName.length; i++) {
-//         // underName[i];
-//         if (underName[i] === "_") {
-//             foundUnder = true;
-//         } else {
-//             if (foundUnder) {
-//                 camelCaseOutput += underName[i].toUpperCase();
-//                 foundUnder = false;
-//             } else {
-//                 camelCaseOutput += underName[i];
-//             }
-//         }
-//     }
+/* Solution 2 */    
+   
+// function underToCamel(name){
+// 	var finalString = "",
+// 		name = name.toLowerCase(),
+// 		count = 0;
+
+// 	for(var i = 0; i < name.length; i++){
+// 		if(name[i] === "_"){
+// 			finalString += name[i+1].toUpperCase();
+// 			count++;
+// 		} else if(finalString[i-count] === name[i].toUpperCase()) {
+// 		    continue;
+// 		} else {
+// 		    finalString += name[i];
+// 		}
+// 	}
+// 	return finalString;
+// }
+
+
 
 //     /* Solution 3 */    
+
+// function underToCamel(underName) {
+//     var camelCaseOutput = "";
 //     // var underWordArray = underName.split("_");
 //     // camelCaseOutput = underWordArray[0].toLowerCase();
 //     // for(var j = 1; j < underWordArray.length; j++) {
