f25de321eceee7b6a6bd9fe5ea581ecb5a825990
Scott D'Alessandro <sdaless22@gmail.com>
2016-05-09 17:55:44 -0400

    Update palindrome.js

--- Problem-Set-3-Solutions/palindrome.js
+++ Problem-Set-3-Solutions/palindrome.js
@@ -13,3 +13,12 @@ function palindrome(str){
   return false;
 }
 
+
+function palindrome(str) {
+  for (var i = 0; i < str.length; i++)
+    if (str[i] !== str[str.length - i - 1]){
+      return false;
+    }
+  return true;
+}
+
